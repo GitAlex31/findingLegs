551e6fb8fda54f13c09079ca9d1a881557c69948
Alexandre Dossin <Alexandre Dossin>
2017-03-28 14:37:11 -0600

    Statistics added about the paths

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import graph, simplePaths
-import random
+import random, math
 
 def timeToDist(time, speed):
     return float(time)*speed
@@ -34,7 +34,7 @@ def test1():
 def test2():
     """Simulation of the enumeration of all the simple paths in a graph with realistic parameter values"""
     nodes = []
-    numberOfCustomers = 4
+    numberOfCustomers = 5
     maxDistance = 1000  # in meters
     explorationTime = 5  # in minutes
     droneSpeed = 600  # in m/min
@@ -53,8 +53,21 @@ def test2():
             if node != other:
                 g.addEdge(graph.Edge(node, other))
 
-    paths = simplePaths.exploreSimplePaths(g, 0, numberOfCustomers - 1, droneSpeed=droneSpeed, toPrint=True)
-    #print("Simple paths : ", [[node.getName() for node in trip] for trip in paths])
+    paths = simplePaths.exploreSimplePaths(g, 0, numberOfCustomers - 1, droneSpeed=droneSpeed, toPrint=False)
+    print("Number of customers : {}".format(numberOfCustomers))
+    print("Size of the square : {} meters".format(maxDistance))
+    print("Number of paths : ", len(paths))
+    min_nbr = min([len(path) - 2 for path in paths])
+    max_nbr = max([len(path) - 2 for path in paths])
+    print("Min number of clients between two depots : {}".format(min_nbr))
+    print("Max number of clients between two depots : {}".format(max_nbr))
+    mean = float(sum([len(path) - 2 for path in paths]))
+    mean /= len(paths)
+    print("Mean number of clients between two depots per leg : {}".format(mean))
+    totalNumberPaths = math.factorial(numberOfCustomers - 2) * sum([1 / math.factorial(k) for k in range(numberOfCustomers - 1)])
+    fracPaths = len(paths) / totalNumberPaths
+    print("Fraction of used paths : {} %".format(fracPaths*100))
+    print("Simple paths : ", [[node.getName() for node in trip] for trip in paths])
 
 
 def main():
