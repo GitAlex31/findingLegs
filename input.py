854edb4885f0137ba7a82196f82d93d91e566130
Alexandre Dossin <Alexandre Dossin>
2017-04-05 14:57:28 -0600

    Code added to write the legs inside a file.

--- input.py
+++ input.py
@@ -6,10 +6,31 @@ def createInputFile(g, fileName, droneSpeed=600, droneAutonomy=25, toPrint= Fals
     A leg is represented by an id, its cost, an origin and a destination depot and a list of visited customers"""
 
     simplePathsList = simplePaths.exploreAllSimplePaths(g, droneSpeed, droneAutonomy, toPrint)
+    print(len(simplePathsList))
     myFile = open(fileName, "w")
-    myFile.write("Number of customers: {}   Number of Depots: {}    Drone Autonomy: {} min  Drone Speed:{} m/s"
+    myFile.write("Number of customers: {}   Number of Depots: {}    Drone Autonomy: {} min  Drone Speed:{} m/min"
                  .format(len(g.getCustomers()), len(g.getDepots()), droneAutonomy, droneSpeed))
+    myFile.write('\n \n')
 
+    # description of the columns
+    myFile.write("leg_id    time    originDepot destinationDepot    visitedNodes")
+    myFile.write('\n')
+
+    # filling the file with legs
+    for i, leg in enumerate(simplePathsList):
+        leg_id = i
+        # making an object of the path is used to compute in a more OOP way the length of that path
+        legObject = graph.Path(leg)
+        time = legObject.computeLength(droneSpeed)
+        visitedNodesStr = str()
+        if len(leg) > 2:
+            visitedNodesStr = " ".join([node.getName() for node in leg[1:-1]])
+
+
+        myFile.write(str(leg_id) + '        ' + str(round(time, 1)) + '         ' + str(leg[0].getName())
+                     + '           ' + str(leg[-1].getName()) + '                   '
+                     '[' + visitedNodesStr + ']')
+        myFile.write('\n')
     pass
 
 
