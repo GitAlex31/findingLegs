91e18801c9644c231e7df88998d12e4abc5d8948
GitAlex31 <alexandre.dossin@outlook.fr>
2017-04-12 13:47:26 -0600

    Moving buildGraph function to graph.py

--- main.py
+++ main.py
@@ -78,59 +78,6 @@ def test2():
         print("Fraction of used paths : {} %".format(fracPaths*100))
         print("Simple paths : ", [[node.getName() for node in trip] for trip in paths])
 
-def buildGraph(numberOfCustomers, numberOfDepots, maxDistance, explorationTime=5):  # TODO : put that in graph.py
-    """Build graph g with user-defined parameter values"""
-
-    g = graph.Digraph()
-
-    customers = []
-    for i in range(1, numberOfCustomers+1):
-        x = random.random() * maxDistance  # square of dimensions maxDistance*maxDistance
-        y = random.random() * maxDistance
-        name = str(i)
-        customers.append(graph.Node(name, x, y, explorationTime))
-
-    depots = []
-    name = numberOfCustomers+1
-    for i in range(numberOfCustomers+1, numberOfCustomers + numberOfDepots+1):
-        x = random.random() * maxDistance  # square of dimensions maxDistance*maxDistance
-        y = random.random() * maxDistance
-        depots.append(graph.Node(str(name), x, y, -1))  # the depot has no exploration time : so -1 by default
-        depots.append(graph.Node(str(name+1), x, y, -1))  # we duplicate each depot
-        name += 2
-
-    # generation of a graph allowing the exploration of the complete graph and also the self circuits
-    nodes = customers + depots
-    for node in nodes:
-        g.addNode(node)
-
-    depotsListForGraphExploration = [depot for depot in depots if depots.index(depot) % 2 == 0]  # TODO : change with the class mehods of graph.py
-    depotsListForSelfLoops = [depot for depot in depots if depots.index(depot) % 2 != 0]
-
-    for customer in customers:  # first, we make a directed clique with the customers set
-        for other in customers:
-            if customer != other:
-                g.addEdge(graph.Edge(customer, other))
-
-    for depot in depotsListForGraphExploration:  # then we add the edges for the graph exploration
-        for customer in customers:
-            g.addEdge(graph.Edge(depot, customer))
-            g.addEdge(graph.Edge(customer, depot))
-        for other in depotsListForGraphExploration:
-            if depot != other:
-                g.addEdge(graph.Edge(depot, other))
-
-    for depot in depotsListForSelfLoops:  # finally we add the edges to allow for self circuits
-        for customer in customers:
-            g.addEdge(graph.Edge(depot, customer))
-
-        # index corresponding to the associated depot for graph exploration
-        correspondingDepotIdx = int(depot.getName()) - 1
-
-        g.addEdge(graph.Edge(depot, g.getNode(correspondingDepotIdx)))
-
-    return g
-
 
 def main():
     #test1()
@@ -138,7 +85,7 @@ def main():
     numberOfCustomers = 1
     numberOfDepots = 2
     if numberOfCustomers >= 1 and numberOfDepots >= 2:
-        g = buildGraph(numberOfCustomers, numberOfDepots, 1000)  # for testing
+        g = graph.buildGraph(numberOfCustomers, numberOfDepots, 1000)  # for testing
     else:
         raise ValueError("The network must have at least 1 customer and 2 depots.")
     #print(g)
