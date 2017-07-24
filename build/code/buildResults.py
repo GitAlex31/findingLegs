27ab53f1dc7fbab9e90378b657c8e6fa8e9014bc
GitAlex31 <alexandre.dossin@outlook.fr>
2017-07-24 14:07:59 -0600

    Removing of the useless buildLatexTables function. Replacement of this function by one that returns a dictionary of results.

---
+++ build/code/buildResults.py
@@ -0,0 +1,55 @@
+# Author : Alexandre Dossin
+
+import matplotlib.pyplot as plt
+
+def buildResultsDictionary(reportFileName):
+    """Build the dictionary of results for a single VrpGencol report file named reportFileName"""
+
+    results = dict()  # initialization of the dictionary of results
+
+    # we save the relevant parameters in variables
+
+    # with open automatically closes the file when not used anymore
+    with open("../input/" + reportFileName, 'r') as reportFile:
+
+        for line in reportFile:
+            if "Best feasible solution" in line:
+                idx1 = line.index(":")
+                idx2 = line.index("(")
+                bestFeasibleSolution = int(float(line[idx1 + 1:idx2].strip()))
+                results['opt'] = bestFeasibleSolution
+            elif "Number of vehicles used" in line and "maximum capacity" not in line:
+                idx = line.index("=")
+                numberOfVehicles = line[idx + 1:-1].strip()
+                results['n_v'] = numberOfVehicles
+            elif "Entire solving process" in line:
+                idx1 = line.index("Time")
+                idx2 = line.index(")")
+                entireTime = line[idx1 + 4:idx2].strip()
+                results['time'] = entireTime
+            elif "Best relaxation cost" in line:
+                idx1 = line.index(":")
+                idx2 = line.index("(")
+                bestRelaxationCost = line[idx1 + 1:idx2].strip()
+                results['lr'] = bestRelaxationCost
+
+    return results
+
+
+def buildAllResultsDictionary(reportFileNameList):
+    """Returns the dictionary containing the dictionaries of results for each
+    report in the VrpGencol report list given."""
+    allResults = dict()
+
+    for reportFileName in reportFileNameList:
+        allResults[reportFileName] = buildResultsDictionary(reportFileName)
+
+    return allResults
+
+def plotResults(reportFileNameList, dependentVariable):
+    """Plot the results on a graph with the dependent variable (y-axis) being specified, e.g. time."""
+    pass
+
+if __name__ == '__main__':
+    reportFileName = input("Please enter VrpGencol report file name :")
+    #buildResultsDictionary(reportFileName)
