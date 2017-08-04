0b78e5f8c8bf7f69184e39d7056daf6501b01292
GitAlex31 <alexandre.dossin@outlook.fr>
2017-08-04 16:37:14 -0600

    Add the possibility to print the LaTeX subtables to integrate to the LaTeX tables for the first approach for quick copy-paste in LaTeX.

--- build/code/buildResults.py
+++ build/code/buildResults.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 def buildResultsDictionary(reportFileName):
     """Build the dictionary of results for a single VrpGencol report file named reportFileName."""
-
     results = dict()
 
     with open("../input/" + reportFileName, 'r') as reportFile:
@@ -29,6 +28,14 @@ def buildResultsDictionary(reportFileName):
                 idx2 = line.index("(")
                 bestRelaxationCost = line[idx1 + 1:idx2].strip()
                 results['lr'] = bestRelaxationCost
+            elif "Nodes" in line:
+                idx1 = line.index(":")
+                numberOfNodes = line[idx1+1:-1].strip()
+                results['nodes'] = numberOfNodes
+            elif "Arcs" in line:
+                idx1 = line.index(":")
+                numberOfArcs = line[idx1+1:-1].strip()
+                results['arcs'] = numberOfArcs
 
     return results
 
@@ -43,14 +50,33 @@ def buildAllResultsDictionary(reportFileNameList):
 
     return allResults
 
+def printLaTeXSubTable(reportFileNameList):
+    """Print the necessary lines of the LaTeX subtable (to integrate in the main table) for quick copy-paste."""
+    numberOfCustomers = 10
+    for reportFileName in reportFileNameList:
+        reportResults = buildResultsDictionary(reportFileName)
+        numberOfNodes = reportResults['nodes']
+        numberOfArcs = reportResults['arcs']
+        optimalSolution = reportResults['opt']
+        numberOfVehicles = reportResults['n_v']
+        runningTime = reportResults['time']
+        linearSolution = reportResults['lr']
+        print("\cline{2-7}")
+        print("& {} & {} & {} & {} ({}) & {} & {}\\\\".format(numberOfCustomers, numberOfNodes, numberOfArcs,
+                                                             optimalSolution, numberOfVehicles, runningTime,
+                                                             linearSolution))
+        numberOfCustomers += 5
+    pass
+
 def plotResults(reportFileNameList, yVar, xVar):
     """Plot the results on a graph with y and x variables specified, e.g. time against number of customers"""
     pass
 
-
 if __name__ == '__main__':
     #reportFileName = input("Please enter VrpGencol report file name :")
-    reportList = ['reportVrp50_2_tight15_as.out']
+    print("ATTENTION : bien classer les fichiers report dans l'ordre croissant des clients.")
+    reportList = ['reportVrp17_5_tight60_s15_a60_as.out', 'reportVrp18_5_tight60_s15_a60_as.out']
     #buildResultsDictionary(reportFileName)
     #print(buildAllResultsDictionary(reportList))
+    printLaTeXSubTable(reportList)
 
