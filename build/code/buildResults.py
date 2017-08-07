74dcfd7ab87196f3c88a1f5ca9e1b3fd9a09cc28
GitAlex31 <alexandre.dossin@outlook.fr>
2017-08-07 15:35:20 -0600

    Update the printing of the LaTeX subtables.

--- build/code/buildResults.py
+++ build/code/buildResults.py
@@ -14,6 +14,8 @@ def buildResultsDictionary(reportFileName):
                 idx2 = line.index("(")
                 bestFeasibleSolution = int(float(line[idx1 + 1:idx2].strip()))
                 results['opt'] = bestFeasibleSolution
+            elif "No feasible solution was found" in line:
+                results['opt'] = "N/A"
             elif "Number of vehicles used" in line and "maximum capacity" not in line:
                 idx = line.index("=")
                 numberOfVehicles = line[idx + 1:-1].strip()
@@ -51,8 +53,10 @@ def buildAllResultsDictionary(reportFileNameList):
     return allResults
 
 def printLaTeXSubTable(reportFileNameList):
-    """Print the necessary lines of the LaTeX subtable (to integrate in the main table) for quick copy-paste."""
-    numberOfCustomers = 10
+    """Print the necessary lines of the LaTeX sub-table (to integrate in the main table) for quick copy-paste."""
+    numberOfCustomers = [10, 15, 20, 25, 30, 35]
+    numberOfCustomersIdx = 0
+
     for reportFileName in reportFileNameList:
         reportResults = buildResultsDictionary(reportFileName)
         numberOfNodes = reportResults['nodes']
@@ -62,10 +66,12 @@ def printLaTeXSubTable(reportFileNameList):
         runningTime = reportResults['time']
         linearSolution = reportResults['lr']
         print("\cline{2-7}")
-        print("& {} & {} & {} & {} ({}) & {} & {}\\\\".format(numberOfCustomers, numberOfNodes, numberOfArcs,
-                                                             optimalSolution, numberOfVehicles, runningTime,
-                                                             linearSolution))
-        numberOfCustomers += 5
+        print("& {} & {} & {} & {} ({}) & {} & {}\\\\".format(numberOfCustomers[numberOfCustomersIdx],
+                                                              numberOfNodes, numberOfArcs, optimalSolution,
+                                                              numberOfVehicles, runningTime, linearSolution))
+        numberOfCustomersIdx += 1
+        if numberOfCustomersIdx == 6:
+            numberOfCustomersIdx = 0
     pass
 
 def plotResults(reportFileNameList, yVar, xVar):
@@ -75,7 +81,16 @@ def plotResults(reportFileNameList, yVar, xVar):
 if __name__ == '__main__':
     #reportFileName = input("Please enter VrpGencol report file name :")
     print("ATTENTION : bien classer les fichiers report dans l'ordre croissant des clients.")
-    reportList = ['reportVrp17_5_tight60_s15_a60_as.out', 'reportVrp18_5_tight60_s15_a60_as.out']
+    # reportList = ['reportVrp10_2_tight15_s45_a60_as.out', 'reportVrp15_2_tight15_s45_a60_as.out',
+    #               'reportVrp20_2_tight15_s45_a60_as.out', 'reportVrp25_2_tight15_s45_a60_as.out',
+    #               'reportVrp30_2_tight15_s45_a60_as.out', 'reportVrp35_2_tight15_s45_a60_as.out']
+    numberOfCustomers = [10, 15, 20, 25, 30, 35]
+    serviceTimes = [45, 30, 20, 15]
+    reportList = []
+    for serviceTime in serviceTimes:
+        for customer in numberOfCustomers:
+            reportList += ['reportVrp{}_2_tight45_s{}_a60_as.out'.format(customer, serviceTime)]
+    print(reportList)
     #buildResultsDictionary(reportFileName)
     #print(buildAllResultsDictionary(reportList))
     printLaTeXSubTable(reportList)
